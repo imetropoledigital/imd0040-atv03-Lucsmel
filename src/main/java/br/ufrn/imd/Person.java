649b6ca81c423e9467299fa6c1e6600170e015c7
lucas <lucsmel@gmail.com>
2024-04-09 22:34:12 -0300

    a

---
+++ src/main/java/br/ufrn/imd/Person.java
@@ -0,0 +1,16 @@
+package br.ufrn.imd;
+
+public class Person {
+    public string name;
+    public string hometown;
+
+    Person() {}
+    public string hometown(){
+        return hometown;
+    }
+    public string name() {
+        return name;
+    }
+
+}
+
